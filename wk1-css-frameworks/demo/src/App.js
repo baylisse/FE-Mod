e19fbb7bf54139f26b90408f3472e20b38cbde2b
Betsy Bayliss <betsy.bayliss@multiverse.io>
2023-02-17 14:28:19 -0800

    adds wk1 demo with comments

--- wk1-css-frameworks/demo/src/App.js
+++ wk1-css-frameworks/demo/src/App.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React from "react";
+import { Container } from "react-bootstrap";
+import Stack from "react-bootstrap/Stack";
+import Dropdown from 'react-bootstrap/Dropdown';
 
-
+import "./styles.css";
 /*
 FYI! 
 first install the library:
@@ -17,11 +20,43 @@ For each component, showcase how you can customize the component via props and u
 */
 
 function App() {
-    return (
-        <>
-            <h1>hello, world</h1>
-        </>
-    );
+  return (
+    <>
+      <Container>
+        <h1>hello, world</h1>
+        <Stack gap={3}>
+          <div className="bg-light border">First item</div>
+          <div className="bg-light border">Second item</div>
+          <div className="bg-light border">Third item</div>
+        </Stack>
+        {/*i'm using bootstrap styles (mt-4 for margin-top) on a regular html element */}
+        <p className='mt-4'>goodbye, world</p>
+        {/*even though this is a horizontal stack, I still had to target it with 
+        a custom className and display: flex; in styles.css to make it appear 
+        horizontally because of competing stylesheets - Bootstrap can be hard to work with! */}
+        <Stack className='horizontalPlz mt-4' direction="horizontal" gap={3}>
+          <div className="bg-light border">First item</div>
+          {/** ms if for margin-start (aka left)*/}
+          <div className="bg-light border ms-auto">Second item</div>
+          <div className="bg-light border">Third item</div>
+        </Stack>
+      </Container>
+      <Container className='mt-4'>
+        <p>another container</p>
+        <Dropdown>
+          <Dropdown.Toggle variant="danger" id="dropdown-basic">
+            Button
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Container>
+    </>
+  );
 }
 
 export { App };
