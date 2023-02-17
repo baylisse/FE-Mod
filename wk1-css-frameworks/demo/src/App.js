c75426c9c3d04b5a3ec27c147d708aa970649907
Betsy Bayliss <betsy.bayliss@multiverse.io>
2023-02-17 08:09:52 -0800

    first commit

---
+++ wk1-css-frameworks/demo/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+/*
+FYI! 
+first install the library:
+npm install react-bootstrap bootstrap
+(install _both_ react-bootstrap (the react components) and bootstrap (the css styles))
+and add the css styles to the project (index.html)
+
+then import each component as you render it
+
+it's not necessary that you use every component here, but here's a bunch of good examples.
+
+For each component, showcase how you can customize the component via props and utilize `className` to further customize the component.
+(this is to drive home the point that we'll typically be using a combination of both a Component and classnames)
+*/
+
+function App() {
+    return (
+        <>
+            <h1>hello, world</h1>
+        </>
+    );
+}
+
+export { App };
